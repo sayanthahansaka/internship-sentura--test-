9ee8a66e30cb6ec6a0078310a731cd863c1e49b2
Sayantha Hansaka <sayanthahansaka00@gmail.com>
2024-02-03 09:29:34 +0530

    first commit

---
+++ src/main/java/com/example/internshipsentura/InternshipSenturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.internshipsentura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipSenturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipSenturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/internshipsentura/InternshipSenturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internshipsentura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipSenturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
